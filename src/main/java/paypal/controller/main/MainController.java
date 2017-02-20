fa9c6f2be4c4eac01e3e51e6d3517c210b9c0496
stu <stu@stu-PC>
2017-02-20 11:26:01 +0900

    first commit

--- src/main/java/paypal/controller/main/MainController.java
+++ src/main/java/paypal/controller/main/MainController.java
@@ -1,15 +1,92 @@
 package paypal.controller.main;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import paypal.dao.main.MainDao;
+import paypal.dto.main.MainTransportTotDto;
+import paypal.service.main.MainService;
 
 @Controller
 public class MainController {
-		
+
+
+	private Gson gson = new Gson();
+	
+	@Autowired
+	@Qualifier(value = "mainService")
+	private MainService service;	
+	
+	@Autowired
+	@Qualifier("mainDao")
+	private MainDao dao;
+	public void setMain(MainDao mainDao) {
+		this.dao = mainDao;
+	}
+	
 	@RequestMapping(value = "/")
 	public String showIndexPage() {
 		
 		return "home";
 	}
+	
+	@CrossOrigin
+	@RequestMapping(value="/returnorderinfo.action", method={RequestMethod.GET,RequestMethod.POST}, produces="text/plain;charset=UTF-8")
+	@ResponseBody
+	public String postConnectedFromAnyMall(String key) {		
+		 System.out.println("키값 들어옴 : " + key);		 
+		 return key;
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value="/getorderinfo.action", method={RequestMethod.GET,RequestMethod.POST})
+	public void orderInfoFromAnyMall(@RequestParam("result") String result){
+		
+	    System.out.println("데이터 들어옴 : " + result);
+   
+	    
+	    if(!result.equals("error")){
+	    	//에러가 아닐때 데이터처리 함
+		    MainTransportTotDto mainTransportTotDto = gson.fromJson(result, MainTransportTotDto.class);	    
+	    	
+		    
+		    System.out.println(mainTransportTotDto.getEmail());
+		    System.out.println(mainTransportTotDto.getGender());
+		    System.out.println(mainTransportTotDto.getPhone());
+		    System.out.println(mainTransportTotDto.getAddress3());
+		    
+	    	service.insertMainTx(mainTransportTotDto);
+			
+	    }
+	    	    
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/paypal/service/main/MainService.java
+++ src/main/java/paypal/service/main/MainService.java
@@ -1,9 +1,49 @@
 package paypal.service.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import paypal.dao.main.MainDao;
+import paypal.dto.main.MainTransportProDto;
+import paypal.dto.main.MainTransportTotDto;
 
 
 @Service(value="mainService")
 public class MainService {
 
+	@Autowired
+	@Qualifier(value = "mainDao")
+	private MainDao mainDao;
+	
+	public void insertMainTx(MainTransportTotDto mainTransportTotDto) {
+
+	    mainDao.insertMember(mainTransportTotDto);
+		mainDao.insertPayInfo(mainTransportTotDto);
+		
+		System.out.println("vvvvv" + mainTransportTotDto.getPay_no());
+		
+		
+	    for (MainTransportProDto mainTransportProDto : mainTransportTotDto.getOrderListInfo()) {
+
+	    	System.out.println(mainTransportProDto.getProductName());
+
+	    	mainTransportProDto.setPay_no(mainTransportTotDto.getPay_no());
+			mainDao.insertProductInfo(mainTransportProDto);
+
+	    }
+
+	}	
+	
 }
+
+
+
+
+
+
+
+
